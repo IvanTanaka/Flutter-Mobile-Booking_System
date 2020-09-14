c7ac54bbba71fc4e3c404227ff0af00fd8ecc901
Ivan Tanaka <tanakaivan97@yahoo.com>
2020-09-14 14:09:36 +0700

    changing color in sharedColors

--- lib/ui/shared_colors.dart
+++ lib/ui/shared_colors.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
-class SharedColors{
+class SharedColors {
+//  NEW COLOR
+  static const LinearGradient gradientOrangeColor = LinearGradient(colors: [
+    Color(0xFFEB8C57),
+    Color(0xCEEB8C57),
+    Color(0x33EB8C57),
+  ]);
+
+  static const Color primaryOrangeColor = Color(0xFFEB8C57);
+  static const Color darkOrangeColor = Color(0xFFD26F38);
+  static const Color lightOrangeColor = Color(0xFFF2AA81);
+  static const Color transparentOrangeColor = Color(0x1AF2AA81);
+
+  static const Color blackColor = Color(0xFF000000);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color white2Color = Color(0xFFF4F4F4);
+  static const Color grayColor = Color(0xFF636363);
+
+//  OLD COLOR
   static const Color primaryColor = Color(0xFFFF7266);
   static const Color accentColor = Color(0xFF47ACFF);
 
@@ -14,7 +32,7 @@ class SharedColors{
 
   static const Color btnTxtColor = Color(0xFFFFFFFF);
 
-  static const Color btnDisabledColor = Color(0xFFAEAEAE  );
+  static const Color btnDisabledColor = Color(0xFFAEAEAE);
 
   static const Color statusWaiting = Color(0xFFFCBA03);
   static const Color statusSuccess = Color(0xFF36BF00);
