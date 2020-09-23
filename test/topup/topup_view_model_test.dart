7bd5833f423cb484b32a08f7a9b784f4d49566da
Vielfade <vielfade@yahoo.com>
2020-09-23 14:10:18 +0700

    adding new test to be fixed

--- test/topup/topup_view_model_test.dart
+++ test/topup/topup_view_model_test.dart
@@ -1,5 +1,6 @@
 import 'package:member_apps/core/models/topup_model.dart';
 import 'package:member_apps/core/services/topup_service.dart';
+import 'package:member_apps/core/viewmodels/views/topup/topup_view_model.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -7,6 +8,7 @@ class MockClient extends Mock implements TopupService {}
 
 void main() {
   final client = MockClient();
+  final viewModel = TopupViewModel();
   var amount = 1000000;
   var topup = {
     "id": "123123",
@@ -18,10 +20,16 @@ void main() {
     "updatedAt": "12-12-2000"
   };
   TopupModel tpup = TopupModel.fromJson(topup);
-  
+
   test("testing top up method", () async {
     when(client.topUp(amount: amount)).thenAnswer((_) async => tpup);
     var result = await client.topUp(amount: amount);
     expect(result.amount, tpup.amount);
   });
+
+  // test("description", () async {
+  //   viewModel.validateTopUp("100.000");
+  //   var result = viewModel.topupButtonIsDisabled;
+  //   expect(result, true);
+  // });
 }
