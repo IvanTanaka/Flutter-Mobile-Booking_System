7f0431a723b70c495135a48459bab384d2ad97c5
Vielfade <vielfade@yahoo.com>
2020-09-23 10:27:43 +0700

    adding topup view model test

---
+++ test/topup/topup_view_model_test.dart
@@ -0,0 +1,27 @@
+import 'package:member_apps/core/models/topup_model.dart';
+import 'package:member_apps/core/services/topup_service.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class MockClient extends Mock implements TopupService {}
+
+void main() {
+  final client = MockClient();
+  var amount = 1000000;
+  var topup = {
+    "id": "123123",
+    "customerId": "asdasdq23",
+    "amount": amount,
+    "vaNumber": "1111111",
+    "status": "test",
+    "createdAt": "12-12-1999",
+    "updatedAt": "12-12-2000"
+  };
+  TopupModel tpup = TopupModel.fromJson(topup);
+  
+  test("testing top up method", () async {
+    when(client.topUp(amount: amount)).thenAnswer((_) async => tpup);
+    var result = await client.topUp(amount: amount);
+    expect(result.amount, tpup.amount);
+  });
+}
